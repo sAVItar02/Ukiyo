cb4c999a11c2395d8cbf04f7aeed2c1c4486240b
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-11-25 20:16:11 +0530

    manage embeds

--- commands/recommendWatch.js
+++ commands/recommendWatch.js
@@ -1,51 +1,72 @@
 const request = require("request");
 const Data = require("../models/userModel");
+const discord = require("discord.js");
 
-module.exports.run = async(bot, message, args) =>{
+module.exports.run = async (bot, message, args) => {
+  if (!args[0].startsWith("<@")) {
+    message.channel.send(
+      "If I were you, I'd recommend that to a user by tagging them 🍜"
+    );
+    return;
+  }
 
-    if(!(args[0]).startsWith('<@')){
-        message.channel.send('If I were you, I\'d recommend that to a user by tagging them 🍜');
-        return;
+  let user = message.author;
+  let tagged = args[0];
+  let taggedID = tagged.slice(3, tagged.length - 1);
+  if (user.id === taggedID) return;
+
+  const url = `https://kitsu.io/api/edge/anime?filter[text]=${args[1]}`;
+
+  request({ url }, async (err, response) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const animeData = JSON.parse(response.body);
+      let anime = {
+        title: animeData.data[0].attributes.titles.en_us,
+        jp: animeData.data[0].attributes.titles.en_jp,
+        image: animeData.data[0].attributes.posterImage.small,
+        desc: animeData.data[0].attributes.description.split(".")[0],
+        status: animeData.data[0].attributes.status,
+        epCount: animeData.data[0].attributes.episodeCount,
+      };
+
+      Data.findOne({ uid: taggedID }, async (err, data) => {
+        if (!data) {
+          const newData = new Data({
+            uid: taggedID,
+            watchList: [],
+            watchLater: [],
+            recommended: [anime.title],
+          });
+          await newData.save().catch((err) => console.log(err));
+        } else {
+          data.recommended.push(anime.title);
+          await data.save().catch((err) => console.log(err));
+          console.log(data);
+        }
+
+        const recommendEmbed = new discord.MessageEmbed()
+          .setColor("#BFFF00")
+          .setAuthor(`Anime recommended`)
+          .setTitle(anime.title)
+          .setDescription(anime.desc)
+          .setThumbnail(anime.image)
+          .addFields(
+            { name: "Status", value: `\`${anime.status}\``, inline: true },
+            {
+              name: "Episode Count",
+              value: `\`${anime.epCount}\``,
+              inline: true,
+            }
+          );
+        message.channel.send(recommendEmbed);
+      });
     }
-
-    let user = message.author;
-    let tagged = args[0];
-    let taggedID = tagged.slice(3,tagged.length-1);
-    if(user.id===taggedID) return;
-
-    const url = `https://kitsu.io/api/edge/anime?filter[text]=${args[1]}`;
-
-    request({url}, async(err, response)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            const anime = JSON.parse(response.body).data[0].attributes.titles.en_us;
-
-            Data.findOne({uid: taggedID}, async(err, data)=>{
-                if(!data){
-                    const newData = new Data({
-                        uid: taggedID,
-                        watchList: [],
-                        watchLater: [],
-                        recommended: [anime], 
-                    });
-                    await newData.save().catch((err)=> console.log(err));
-                    message.channel.send('Anime recommended!');
-                }else{
-                    data.recommended.push(anime);
-                    await data.save().catch((err) => console.log(err));
-                    console.log(data);
-                    message.channel.send('Anime recommended!');
-                }   
-            });
-        }
-    });
-}
-
+  });
+};
 
 module.exports.help = {
-    name: "recommend",
-    aliases: ['r','rec','rmd',],
-  };
-  
+  name: "recommend",
+  aliases: ["r", "rec", "rmd"],
+};
--- commands/setWatchList.js
+++ commands/setWatchList.js
@@ -1,10 +1,11 @@
 const request = require("request");
 const Data = require("../models/userModel");
+const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let user = message.author;
-  if(args.length>1){
-    args=args.join('-');
+  if (args.length > 1) {
+    args = args.join("-");
   }
   console.log(args);
   const url = `https://kitsu.io/api/edge/anime?filter[text]=${args}`;
@@ -14,31 +15,54 @@ module.exports.run = async (bot, message, args) => {
     if (err) {
       console.log(err);
     } else {
-      const anime = JSON.parse(response.body).data[0].attributes.titles.en_us;
+      const animeData = JSON.parse(response.body);
+      let anime = {
+        title: animeData.data[0].attributes.titles.en_us,
+        jp: animeData.data[0].attributes.titles.en_jp,
+        image: animeData.data[0].attributes.posterImage.small,
+        desc: animeData.data[0].attributes.description.split(".")[0],
+        status: animeData.data[0].attributes.status,
+        epCount: animeData.data[0].attributes.episodeCount,
+      };
       Data.findOne({ uid: user.id }, async (err, data) => {
         if (!data) {
           const newData = new Data({
             uid: user.id,
             name: bot.users.cache.get(user.id).username,
-            watchList: [anime],
+            watchList: [anime.title],
             watchLater: [],
             recommended: [],
           });
 
           await newData.save().catch((e) => console.log(e));
-          message.channel.send("Anime added!");
         } else {
-          data.watchList.push(anime);
+          data.watchList.push(anime.title);
           await data.save().catch((e) => console.log(e));
           console.log(data);
-          message.channel.send("Anime added!");
+          // message.channel.send("Anime added!");
         }
+        const animeEmbed = new discord.MessageEmbed()
+          .setColor("#BFFF00")
+          .setAuthor("Anime Added!")
+          .setTitle(anime.title)
+          .setDescription(anime.desc)
+          .setThumbnail(anime.image)
+          .addFields(
+            { name: "Status", value: `\`${anime.status}\``, inline: true },
+            {
+              name: "Episode Count",
+              value: `\`${anime.epCount}\``,
+              inline: true,
+            }
+          );
+
+        message.channel.send(animeEmbed);
       });
     }
   });
 };
 
 module.exports.help = {
-  name: 'watching',
-  aliases: ['w','seeing','watch'],
+  name: "watching",
+  aliases: ["w", "seeing", "watch", "add"],
 };
