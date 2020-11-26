474411e67f7d2133082fac14c692520847c9c74e
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-11-27 03:02:56 +0530

    Help commands

--- commands/getWatchLater.js
+++ commands/getWatchLater.js
@@ -1,11 +1,11 @@
-const Data = require('../models/userModel');
-const discord = require('discord.js');
-const pagination = require('discord.js-pagination');
+const Data = require("../models/userModel");
+const discord = require("discord.js");
+const pagination = require("discord.js-pagination");
 
 module.exports.run = async (bot, message, args) => {
   let user;
   let pages = [];
-  if (args[0] && args[0].startsWith('<@')) {
+  if (args[0] && args[0].startsWith("<@")) {
     user = message.mentions.users.first();
   } else {
     user = message.author;
@@ -21,27 +21,27 @@ module.exports.run = async (bot, message, args) => {
       const watchLater = data.watchLater;
       let embed = new discord.MessageEmbed();
       for (i = 0; i < watchLater.length; ) {
-        let genre='';
-        watchLater[i].genres.forEach(element => {
-          genre+=`\`${element}\` `
+        let genre = "";
+        watchLater[i].genres.forEach((element) => {
+          genre += `\`${element}\` `;
         });
         if (i != 0 && i % 5 == 0) {
           pages.push(embed);
           embed = new discord.MessageEmbed();
         }
         embed
-        .setColor('#f2cd55')
-        .addField(`${i + 1}. ${watchLater[i++].anime}`, `${genre}`);
+          .setColor("#f2cd55")
+          .addField(`${i + 1}. ${watchLater[i++].anime}`, `${genre}`);
       }
       pages.push(embed);
     }
-    const emojiList = ['⏮', '⏭'];
+    const emojiList = ["⏮", "⏭"];
     const timeOut = 200000;
     pagination(message, pages, emojiList, timeOut);
   });
 };
 
 module.exports.help = {
-  name: 'viewWatchLater',
-  aliases: ['vwl', 'view-wl', 'vwatchlaterlist', 'vwatl'],
+  name: "viewWatchLater",
+  aliases: ["vwl", "view-wl", "vwlater", "vwatchl"],
 };
--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,130 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  const Embed = new discord.MessageEmbed()
-    .setAuthor("Konnichiwa uwu")
-    .setColor("#FFB6C1")
-    .setTitle("Help")
-    .setDescription("Some commands for your help 💥")
-    // .setThumbnail(
-    //   "https://www.awesomeinventions.com/wp-content/uploads/2015/06/japanese-words-ukiyo.jpg"
-    // )
-    .addFields(
-      { name: "👓Recommend", value: `\`recommend\``, inline: true },
-      { name: "🍿Watch Later", value: `\`watch\``, inline: true },
-      { name: "✨Watch Now", value: `\`add\``, inline: true },
-      { name: "📺Anime", value: `\`get\``, inline: true },
-      { name: "❓Trivia", value: `\`trivia\``, inline: true }
-    );
-  message.channel.send(Embed);
+  if (!args[0]) {
+    const Embed = new discord.MessageEmbed()
+      .setAuthor("Konnichiwa uwu")
+      .setColor("#FFFE00")
+      .setTitle("Help")
+      .setDescription("Some commands for your help 💥")
+      // .setThumbnail(
+      //   "https://www.awesomeinventions.com/wp-content/uploads/2015/06/japanese-words-ukiyo.jpg"
+      // )
+      .addFields(
+        { name: "👓Recommend", value: `\`uki help recommend\``, inline: true },
+        { name: "🍿Watch Later", value: `\`uki help wlater\``, inline: true },
+        { name: "✨Watching Now", value: `\`uki help watch\``, inline: true },
+        { name: "🔍Anime", value: `\`uki help desc\``, inline: true },
+        { name: "❓Trivia", value: `\`uki trivia\``, inline: true },
+        { name: "🏓Ping?", value: `\`uki ping\``, inline: true },
+        {
+          name: "🔴 Remove recommended",
+          value: `\`uki help remrec\``,
+          inline: true,
+        },
+        {
+          name: "🛑 Remove watch later",
+          value: `\`uki help remwl\``,
+          inline: true,
+        },
+        { name: "⛔ Remove watching", value: `\`uki help remw\``, inline: true }
+      );
+    message.channel.send(Embed);
+  } else if (args[0] === "recommend") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("👓Recommend commands!")
+      .setDescription(
+        "For recommending a user: `uki <alias> <anime> <@user>` \n\n For viewing a user's list: `uki <alias> <@user>(optional)`"
+      )
+      .addFields(
+        {
+          name: "🙌Recommend anime to someone",
+          value: "Aliases: `r`, `rec`, `rmd`",
+        },
+        {
+          name: "🕵️‍♂️View recommended anime list",
+          value: "Aliases: `vrl`, `vrecl`, `vreclist`",
+        }
+      );
+    message.channel.send(embed);
+  } else if (args[0] === "wlater") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("⏲Watch Later commands!")
+      .setDescription(
+        "For adding to watch later: `uki <alias> <anime>` \n\n For viewing a user's list: `uki <alias> <@user>(optional)`"
+      )
+      .addFields(
+        {
+          name: "➕Add anime to watch later",
+          value: "Aliases: `wlater`, `wl`, `l`",
+        },
+        {
+          name: "🔎View watch later list",
+          value: "Aliases: `vwl`, `vwlater`, `vwatchl`",
+        }
+      );
+    message.channel.send(embed);
+  } else if (args[0] === "watch") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("😎Watching commands!")
+      .setDescription(
+        "For adding to watching list: `uki <alias> <anime>` \n\n For viewing a user's list: `uki <alias> <@user>(optional)`"
+      )
+      .addFields(
+        {
+          name: "Add anime to watching list",
+          value: "Aliases: `w`, `seeing`, `watch`, `add`",
+        },
+        {
+          name: "🔎View watching list",
+          value: "Aliases: `vnow`, `vcurrent`, `vwatchnow`",
+        }
+      );
+    message.channel.send(embed);
+  } else if (args[0] === "desc") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("📜Describe commands!")
+      .setDescription("For getting anime details: `uki <alias> <anime>`")
+      .addFields({
+        name: "✍Describe any anime",
+        value: "Aliases: `describe`, `desc`, `d`",
+      });
+    message.channel.send(embed);
+  } else if (args[0] === "remrec") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("❗Removing commands!")
+      .setDescription("For removing from recommended: `uki <alias> <anime>`")
+      .addFields({
+        name: "🟥Remove from recommended",
+        value: "Aliases: `rrec`, `remrec`, `rmr`, `rr`",
+      });
+    message.channel.send(embed);
+  } else if (args[0] === "remw") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("❗Removing commands!")
+      .setDescription("For removing from watching: `uki <alias> <anime>`")
+      .addFields({
+        name: "🟥Remove from watching list",
+        value: "Aliases: `rw`, `remw`, `rmw`",
+      });
+    message.channel.send(embed);
+  } else if (args[0] === "remwl") {
+    const embed = new discord.MessageEmbed()
+      .setColor("#FFFE00")
+      .setTitle("❗Removing commands!")
+      .setDescription("For removing from watch later: `uki <alias> <anime>`")
+      .addFields({
+        name: "🟥Remove from watch later",
+        value: "Aliases: `remwl`, `rmwl`, `rwl`",
+      });
+    message.channel.send(embed);
+  }
 };
 
 module.exports.help = {
