4bfcbcc9063f164d310272dbc8764b9719a61556
Shresth <shresth.tiwary2019@vitstudent.ac.in>
2020-11-28 02:19:12 +0530

    Update topTrending.js
    Change alias name

--- commands/topTrending.js
+++ commands/topTrending.js
@@ -58,5 +58,5 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: 'trending',
-  aliases: ['trend', 'trending', 'trnding'],
+  aliases: ['trend', 'trending', 'hot'],
 };
