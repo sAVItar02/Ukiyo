fe460787b34de913c7e136968b89f69244fb9860
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-12-02 16:40:11 +0530

    getRandomGenre fixes

--- commands/getFromGenre.js
+++ commands/getFromGenre.js
@@ -11,70 +11,123 @@ module.exports.run = async (bot, message, args) => {
     return;
   }
 
-  let variables = {
-    perPage: 5000,
-    genre: args[0],
+  args = args.join(" ");
+
+  const pageQuery = `query ($search: String, $page: Int, $perPage: Int, $genre:String) {
+    Page (page: $page, perPage: $perPage) {
+      pageInfo {
+        total
+        currentPage
+        lastPage
+        hasNextPage
+        perPage
+      }
+      media (search: $search, type: ANIME, genre: $genre, sort: POPULARITY_DESC) {
+      id
+      }
+    }
+  }`;
+
+  let pageVariables = {
+    perPage: 50,
+    genre: args,
+    page: 1,
   };
 
-  let options = {
+  let pageOptions = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
     body: JSON.stringify({
-      query: query,
-      variables: variables,
+      query: pageQuery,
+      variables: pageVariables,
     }),
   };
 
-  fetch(url, options)
+  let maxPages = 1;
+
+  fetch(url, pageOptions)
     .then((response) => response.json())
     .then((result) => {
-      const animeData =
-        result.data.Page.media[
-          Math.floor(Math.random() * result.data.Page.media.length)
-        ];
-      let anime = {
-        title: animeData.title.romaji,
-        english: animeData.title.english,
-        image: animeData.coverImage.large,
-        desc: animeData.description,
-        status: animeData.status,
-        epCount: animeData.episodes,
-        duration: animeData.duration,
-        genres: animeData.genres,
-      };
-      let genre = "";
-      anime.genres.forEach((element) => {
-        genre += `\`${element}\` `;
-      });
-      const animeEmbed = new discord.MessageEmbed()
-        .setColor("#EE8BC1")
-        .setTitle(anime.title)
-        .setDescription(anime.desc)
-        .setThumbnail(anime.image)
-        .addFields(
-          { name: "Status", value: `\`${anime.status}\``, inline: true },
-          {
-            name: "Episode Count",
-            value: `\`${anime.epCount}\``,
-            inline: true,
-          },
-          {
-            name: "Duration",
-            value: `\`${anime.duration}m\``,
-            inline: true,
-          },
-          {
-            name: "Genre",
-            value: `${genre}`,
-            inline: true,
-          }
-        );
-
-      message.channel.send(animeEmbed);
+      maxPages = result.data.Page.pageInfo.lastPage - 1;
+      if (result.data.Page.pageInfo.total == 0) {
+        message.channel.send("Try another genre! 😐");
+        return;
+      }
+      GetRandomGenre();
     });
+
+  function GetRandomGenre() {
+    const randomPage = Math.floor(Math.random() * maxPages);
+
+    let variables = {
+      perPage: 50,
+      genre: args,
+      page: randomPage,
+    };
+
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
+    };
+
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((result) => {
+        const animeData =
+          result.data.Page.media[
+            Math.floor(Math.random() * result.data.Page.media.length)
+          ];
+        let anime = {
+          title: animeData.title.romaji,
+          english: animeData.title.english,
+          image: animeData.coverImage.large,
+          desc: animeData.description,
+          status: animeData.status,
+          epCount: animeData.episodes,
+          duration: animeData.duration,
+          genres: animeData.genres,
+        };
+        let genre = "";
+        anime.genres.forEach((element) => {
+          genre += `\`${element}\` `;
+        });
+        const animeEmbed = new discord.MessageEmbed()
+          .setColor("#EE8BC1")
+          .setTitle(anime.title)
+          .setDescription(anime.desc)
+          .setThumbnail(anime.image)
+          .addFields(
+            { name: "Status", value: `\`${anime.status}\``, inline: true },
+            {
+              name: "Episode Count",
+              value: `\`${anime.epCount}\``,
+              inline: true,
+            },
+            {
+              name: "Duration",
+              value: `\`${anime.duration}m\``,
+              inline: true,
+            },
+            {
+              name: "Genre",
+              value: `${genre}`,
+              inline: true,
+            }
+          );
+
+        message.channel.send(animeEmbed);
+      });
+  }
 };
 
 module.exports.help = {
